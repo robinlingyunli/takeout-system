f622cf39648fe1cb06b7b68af1d1621be05ffac4
lla272 <lla272@sfu.ca>
2024-05-07 15:42:49 -0700

    initial commit; initialize project structure

---
+++ src/main/java/com/example/reggie/ReggieApplication.java
@@ -0,0 +1,15 @@
+package com.example.reggie;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//用来输出log注解
+@Slf4j
+@SpringBootApplication
+public class ReggieApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReggieApplication.class,args);
+        log.info("项目启动成功...");
+    }
+}
