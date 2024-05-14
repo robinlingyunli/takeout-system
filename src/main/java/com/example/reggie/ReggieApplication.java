5ddef312fc096c51861cf0dae362cc896f1d27f7
lla272 <lla272@sfu.ca>
2024-05-13 17:27:13 -0700

    add login check filter

--- src/main/java/com/example/reggie/ReggieApplication.java
+++ src/main/java/com/example/reggie/ReggieApplication.java
@@ -3,10 +3,12 @@ package com.example.reggie;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 //用来输出log注解
 @Slf4j
 @SpringBootApplication
+@ServletComponentScan
 public class ReggieApplication {
     public static void main(String[] args) {
         SpringApplication.run(ReggieApplication.class,args);
