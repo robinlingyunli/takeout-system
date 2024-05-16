10b2bbc0c636eada7484278780c9ff0aa7d05aeb
lla272 <lla272@sfu.ca>
2024-05-16 03:53:31 -0700

    add employee enable/disable; add employee info update; add Automatic filling of common user fields

--- src/main/java/com/example/reggie/controller/EmployeeController.java
+++ src/main/java/com/example/reggie/controller/EmployeeController.java
@@ -93,18 +93,9 @@ public class EmployeeController {
 
     @PostMapping
     public  R<String> save(HttpServletRequest request, @RequestBody Employee employee){
-        log.info("New employee: {}", employee.toString());
+        log.info("新增员工，员工信息：{}", employee.toString());
         //设置初始密码123456，需要进行md5加密处理
         employee.setPassword(DigestUtils.md5DigestAsHex("123456".getBytes()));
-
-        employee.setCreateTime(LocalDateTime.now());
-        employee.setUpdateTime(LocalDateTime.now());
-
-        //获得当前登录用户的id
-        Long empId = (Long) request.getSession().getAttribute("employee");
-
-        employee.setCreateUser(empId);
-        employee.setUpdateUser(empId);
 
         employeeService.save(employee);
 
@@ -140,5 +131,37 @@ public class EmployeeController {
         employeeService.page(pageInfo,queryWrapper);
 
         return R.success(pageInfo);
+    }
+
+    /**
+     * 根据id修改员工信息
+     * @param employee
+     * @return
+     */
+    @PutMapping
+    public R<String> update(HttpServletRequest request,@RequestBody Employee employee){
+        log.info(employee.toString());
+
+        long id = Thread.currentThread().getId();
+        log.info("employee里, 线程id为：{}",id);
+        
+        employeeService.updateById(employee);
+
+        return R.success("员工信息修改成功");
+    }
+
+    /**
+     * 根据id查询员工信息
+     * @param id
+     * @return
+     */
+    @GetMapping("/{id}")
+    public R<Employee> getById(@PathVariable Long id){
+        log.info("根据id查询员工信息...");
+        Employee employee = employeeService.getById(id);
+        if(employee != null){
+            return R.success(employee);
+        }
+        return R.error("没有查询到对应员工信息");
     }
 }
